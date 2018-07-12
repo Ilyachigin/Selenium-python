4762f939139614432becf4f49b9b3cb522ab8a05
Chigin Ilya <ilya.chigin@mail.msk>
2018-07-12 18:42:01 +0300

    Simple+straight old integration tests

---
+++ Flintmedia/Leadrock/Integration/simple_old.py
@@ -0,0 +1,99 @@
+import re
+import unittest
+import configparser
+from time import sleep
+from random import choice
+from random import randint
+from selenium import webdriver
+from string import ascii_uppercase
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+class IntegrationLeadrock(unittest.TestCase):
+
+    #Driver setup
+    def setUp(self):
+        self.driver = webdriver.Remote(
+            command_executor='http://localhost:4444/wd/hub',
+            desired_capabilities=DesiredCapabilities.CHROME)
+        self.driver.maximize_window()
+
+    def test_integration(self):
+        driver = self.driver
+        byxpath = driver.find_element_by_xpath
+        byid = driver.find_element_by_id
+        byname = driver.find_element_by_name
+        driver.get(config('Site', 'url2'))
+        login(driver, config('Login', 'email2'), config('Login', 'password2'))
+        driver.get(config('Site', 'url2') + '/webmaster/offer/offer/view/id/8')
+    #Url parsing
+        simple = byxpath('//*[@id="subscribe-list"]/table/tbody/tr[3]/td[2]').text
+        if "Обычная старая" in simple:
+            simple_url = byxpath('//*[@id="subscribe-list"]/table/tbody/tr[3]/td[5]/span').text
+    #Relogin to administrator
+        driver.get(config('Site', 'url2') + '/user/authorization/logout')
+        login(driver, config('Login', 'email'), config('Login', 'password'))
+    #Simple landing + checking lead availability
+        print('Simple landing test')
+        driver.get(simple_url)
+        current = driver.current_url
+        trkid = re.search(r'\=([\d]*)', current)
+        name = 'TEST_' + random_word(6)
+        byname('name').send_keys(name)
+        byname('address').send_keys('TEST_' + random_word(10))
+        byname('city').send_keys('TEST_' + random_word(5))
+        byname('pcode').send_keys(str(random_number(5)))
+        byname('phone').send_keys(str(random_number(11)))
+        sleep(2)
+        byid('submit-button').click()
+    #Simple checking
+        sleep(2)
+        confirm = driver.current_url
+        if "confirm" in str(confirm):
+            print('  ...OK!')
+            sleep(3)
+            driver.get(config('Site', 'url2') + '/administrator/lead')
+            trkid_check = byxpath('//*[@id="lead-list"]/table/tbody/tr[1]/td[2]/a').text
+            if str(trkid.group(1)) in str(trkid_check):
+                print('Simple lead integration OK!\n')
+            else:
+                print('NO LEAD!!')
+                driver.quit()
+        else:
+            print('Confirm page ERROR!!')
+            driver.quit()
+
+    #Steps after test
+    def tearDown(self):
+        sleep(5)
+        self.driver.close()
+
+#Random data
+def random_number(n):
+    start = 10**(n-1)
+    end = (10**n)-1
+    return randint(start, end)
+def random_word(m):
+    return ''.join(choice(ascii_uppercase) for i in range(m))
+
+#Common login
+def login(driver, email, password):
+    try:
+        driver.find_element_by_link_text('Login').click()
+    except Exception:
+        driver.find_element_by_link_text('Войти').click()
+    sleep(2)
+    driver.find_element_by_id('LoginForm_username').send_keys(email)
+    driver.find_element_by_id('LoginForm_password').send_keys(password, Keys.ENTER)
+    sleep(3)
+
+#Config data
+def config(section, title):
+    config = configparser.ConfigParser()
+    config.read('../../../settings.ini')
+    config.sections()
+    key = config[section][title]
+    return key
+
+if __name__ == "__main__":
+    unittest.main()
